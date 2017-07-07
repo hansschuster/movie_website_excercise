4bb1b22e6ffb59820e3f96009c2f2ffdc4a69546
hansschuster <johannes.huster@web.de>
2017-07-07 12:24:34 +0200

    Update media.py
    Added a comment

--- media.py
+++ media.py
@@ -5,6 +5,7 @@ import webbrowser
 class Movie():
     """This class provides a way to store movie related information"""
 
+    # Initialise instance of class
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
         self.title = movie_title
